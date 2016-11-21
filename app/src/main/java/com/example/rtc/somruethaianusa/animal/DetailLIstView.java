017ba4ebfc66c82c00dd4565c3b57a9e524f8cf3
masterUNG <phrombutr@gmail.com>
2016-11-21 16:59:35 +0700

    Finish 21nov

--- app/src/main/java/com/example/rtc/somruethaianusa/animal/DetailLIstView.java
+++ app/src/main/java/com/example/rtc/somruethaianusa/animal/DetailLIstView.java
@@ -1,8 +1,10 @@
 package com.example.rtc.somruethaianusa.animal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import org.json.JSONArray;
 
 public class DetailLIstView extends AppCompatActivity {
 
@@ -20,6 +22,15 @@ public class DetailLIstView extends AppCompatActivity {
             getAnimal.execute();
             String strJSON = getAnimal.get();
             Log.d("21novV2", "JSoN ==> " + strJSON);
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+
+            String[] nameStrings = new String[jsonArray.length()];
+            String[] detailStrings = new String[jsonArray.length()];
+            String[] imageStrings = new String[jsonArray.length()];
+            String[] soundStrings = new String[jsonArray.length()];
+
+
 
         } catch (Exception e) {
             Log.d("21novV2", "e onCreate ==> " + e.toString());
