ed772fae067dc6846fa4b19fc9e30e0deef11e74
masterUNG <phrombutr@gmail.com>
2016-11-21 14:01:23 +0700

    Intent ListView OK

---
+++ app/src/main/java/com/example/rtc/somruethaianusa/animal/DetailLIstView.java
@@ -0,0 +1,14 @@
+package com.example.rtc.somruethaianusa.animal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailLIstView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_list_view);
+    }   // Main Method
+
+}   // Main Class
--- app/src/main/java/com/example/rtc/somruethaianusa/animal/MainActivity.java
+++ app/src/main/java/com/example/rtc/somruethaianusa/animal/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.rtc.somruethaianusa.animal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private TextView cat1TextView, cat2TextView, cat3TextView, cat4TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        cat1TextView = (TextView) findViewById(R.id.textView2);
+        cat2TextView = (TextView) findViewById(R.id.textView3);
+        cat3TextView = (TextView) findViewById(R.id.textView4);
+        cat4TextView = (TextView) findViewById(R.id.textView5);
+
+        //TextView Controller
+        cat1TextView.setOnClickListener(this);
+        cat2TextView.setOnClickListener(this);
+        cat3TextView.setOnClickListener(this);
+        cat4TextView.setOnClickListener(this);
+
+
+    }   // Main Method
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.textView2:
+                break;
+            case R.id.textView3:
+
+                Intent intent = new Intent(MainActivity.this, DetailLIstView.class);
+                startActivity(intent);
+
+                break;
+            case R.id.textView4:
+                break;
+            case R.id.textView5:
+                break;
+
+        }   // switch
+
+    }   // onClick
+}   // Main Class
